970777fe9103481de53f5b1c6c2461d3054ea264
Yik11Yueuii <yueyuezi37@gmail.com>
2026-08-29 15:45:19 +0800

    fix: redirect root path to user homepage

--- src/main/java/com/example/demo4/config/WebMvcConfig.java
+++ src/main/java/com/example/demo4/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.example.demo4.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,6 +12,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     public WebMvcConfig(AuthInterceptor authInterceptor) {
         this.authInterceptor = authInterceptor;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/pages/user/index.html");
     }
 
     @Override
